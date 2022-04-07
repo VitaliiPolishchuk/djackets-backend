e91a19b3e00309dafcedb7f367aa3164179418fa
Vitalii <vitalii0polishchuk@gmail.com>
2022-04-07 13:00:34 +0300

    added url into cors allowed origins

--- djackets_django/settings.py
+++ djackets_django/settings.py
@@ -53,7 +53,7 @@ INSTALLED_APPS = [
 
 CORS_ALLOWED_ORIGINS =[
     "http://localhost:8080",
-    # 'http://127.0.0.1:8080',
+    'https://djackets-vitalii-polishchuk.herokuapp.com',
     # 'http://127.0.0.1',
 ]
 
